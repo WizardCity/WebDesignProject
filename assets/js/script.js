8826963dd64b51776c97b52aebb47ef08677f2dd
WizardCity <twohats9001@gmail.com>
2018-04-13 04:13:39 +0100

    Major Update: Products page altered with heavey CSS changes. Added more fields to contact page also, along with an Alert response when User hits submit, along with a redirect to the home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,36 @@
+// Fade in nav buttons on load
 $(document).ready(function(){
-    $(".navbar").hide(0).fadeIn(2000)
+    $(".navbar").hide(0).fadeIn(1000);
+    //Fade in product nav bar slower than main Nav
+    $(".ProductNav").hide(0).delay(200).fadeIn(1500);
 });
 
+// Fades out top bar as user scrolls
 $(window).scroll(function(){
-    $(".top").css("opacity", 1 - $(window).scrollTop() / 250);
+    $(".top").css("opacity", 1 - $(window).scrollTop() / 150);
   });
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function formAlertFunction() {
+	//Alert User that form was completed successfully
+    alert("Your message has been sent to us.\nThank you");
+
+    //redirect after confirmation to home page
+    window.location.href = "index.html";
+}
